d76ad31a9e6e2db641d121d398515bab580047ba
Saad Shahzad <spider32277223@gmail.com>
2020-04-06 15:59:53 -0400

    Converting packets to scapy packets

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import netfilterqueue
+import scapy.all as scapy
 
 def process_packet(packet):
-    print(packet)
-    packet.accept()
+    scapy_packet = scapy.IP(packet.get_payload())  # converting packet to scapy packet i.e. IP layer
+    print(scapy_packet.show())  # print details of the scapy_packet
+    packet.accept()  # accept packets
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
-queue.run()
+queue = netfilterqueue.NetfilterQueue()  # Creating instance of netfilterqueue object
+queue.bind(0,process_packet)  # to bind with the connceted queue. The 0 represents the queue no & the process_packet is
+# the call back function
+queue.run() # to run the function
